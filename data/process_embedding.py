62ce7fdf77170a04e42c47c086c49a0afedf95e9
笹沢裕一 <werimagin@gmail.com>
2019-06-03 15:25:32 +0900

    add

--- data/process_embedding.py
+++ data/process_embedding.py
@@ -3,6 +3,7 @@ import pickle as pkl
 import os
 from tqdm import tqdm
 
+print '########## Embedding processing start ##########\n'
 
 # SETTINGS >>>
 
@@ -12,7 +13,7 @@ dic_dir = 'processed/mpqg_substitute_a_vocab_include_a/'
 dic_name = 'vocab.dic'
 embedding_name = 'glove840b_vocab300.npy'
 
-data_dir = '../../data/'
+data_dir = 'GloVe/'
 glove = 'glove.840B.300d'
 
 
@@ -23,26 +24,31 @@ glove = 'glove.840B.300d'
 
 # LOAD & PROCESS GloVe >>>
 
-print("here")
 
 if not os.path.exists('processed/'+ glove + '.dic.npy'):
     # Load GloVe
+    print '>>> Reading GloVe file...',
     f = open(data_dir + glove + '.txt')
     lines = f.readlines()
     f.close()
+    print 'Complete\n'
 
     # Process GloVe
+    print '>>> Processing Glove...'
     embedding = dict()
     for line in tqdm(lines):
         splited = line.split()
-        embedding[" ".join(splited[0:-300])] = [float(s) for s in splited[-300:]]
+        embedding[splited[0]] = map(float, splited[1:])
 
     # Save processed GloVe as dic file
-    #np.save('processed/' + glove + '.dic', embedding)
+    print '>>> Saving processed GloVe...'
+    np.save('processed/' + glove + '.dic', embedding)
+    print 'Complete\n'
 else:
-    print("here")
+    print 'Processed GloVe exists!'
+    print '>>> Loading processed GloVe file...'
     embedding = np.load('processed/' + glove + '.dic.npy').item()
-
+    print 'Complete\n'
 
 
 
@@ -54,17 +60,13 @@ else:
 
 
 # Load vocabulary
-
-print("here")
-
-with open(os.path.join(dic_dir, dic_name),"rb") as f:
+print '>>> Loading vocabulary...',
+with open(os.path.join(dic_dir, dic_name)) as f:
     vocab = pkl.load(f)
-
-print("here")
-
+print 'Complete\n'
 
 # Initialize random embedding and extract pre-trained embedding
-
+print '>>> Producing pre-trained embedding...',
 embedding_vocab =  np.random.ranf((len(vocab), 300)) -  np.random.ranf((len(vocab), 300))
 
 embedding_vocab[0] = 0.0 # vocab['<PAD>'] = 0
@@ -72,16 +74,22 @@ embedding_vocab[1] = embedding['<s>'] # vocab['<GO>'] = 1
 embedding_vocab[2] = embedding['EOS'] # vocab['<EOS>'] = 2
 embedding_vocab[3] = embedding['UNKNOWN'] # vocab['<UNK>'] = 3
 
-print("here")
-
 unk_num = 0
-for word, idx in tqdm(vocab.items()):
+for word, idx in vocab.items():
     if word in embedding:
         embedding_vocab[idx] = embedding[word]
     else:
         unk_num += 1
+print 'Complete\n'
 
+# Save embedding
+print '>>> Saving pre-trained embedding',
 np.save(os.path.join(dic_dir, embedding_name), embedding_vocab)
+print 'Complete\n'
+
+print '---------- Statistics ----------'
+print 'Vocabulary size: %d'%len(embedding_vocab)
+print 'Unknown words: %d'%unk_num
 
 
 
