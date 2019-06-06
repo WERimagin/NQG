3d0f2625cd1f577e91dd0a467ca21f77f646580f
笹沢裕一 <werimagin@gmail.com>
2019-06-06 23:43:13 +0900

    add

--- data/process_mpqg_data.py
+++ data/process_mpqg_data.py
@@ -51,12 +51,12 @@ if 0:
     test_file_sentence=os.path.join(origin_path,"squad-src-test-normal.txt")
     test_file_question=os.path.join(origin_path,"squad-tgt-test-normal.txt")
     test_file_answer=os.path.join(origin_path,"squad-ans-test-normal.txt")
-if 0:
+if 1:
     setting="full-normal"
     test_file_sentence=os.path.join(origin_path,"squad-src-train-{}.txt".format(setting))
     test_file_question=os.path.join(origin_path,"squad-tgt-train-{}.txt".format(setting))
     test_file_answer=os.path.join(origin_path,"squad-ans-train-{}.txt".format(setting))
-if 1:
+if 0:
     setting="full-normal"
     test_file_sentence=os.path.join(origin_path,"squad-src-dev-{}.txt".format(setting))
     test_file_question=os.path.join(origin_path,"squad-tgt-dev-{}.txt".format(setting))
